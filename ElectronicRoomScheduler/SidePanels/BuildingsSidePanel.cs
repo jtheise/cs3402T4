72f1217e265054684fa5bb6adf2caad69ef7f101
KJ Jablonowski <kjablon@bgsu.edu>
2013-04-22 13:46:05 -0400

    Building side panel comments

--- ElectronicRoomScheduler/SidePanels/BuildingsSidePanel.cs
+++ ElectronicRoomScheduler/SidePanels/BuildingsSidePanel.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* CS 3240 Electronic Room Scheduler
+ * Coded: Rob Risner
+ * Commented: KJ Jablonowski
+ * Due: 4/22/2013
+ * Building Side Menu
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -11,34 +18,40 @@ namespace ElectronicRoomScheduler.SidePanels
 {
     public partial class BuildingsSidePanel : UserControl
     {
+        //create menu panel
         public BuildingsSidePanel()
         {
             InitializeComponent();
         }
 
+        //Add Building link function
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Name, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Name, "Click" }); //save log data
             Program.GetParent().LoadScreen("AddBuilding");
         }
 
+        //Edit Building link function
         private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Name, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Name, "Click" }); //save log data
             Program.GetParent().LoadScreen("EditBuilding");
         }
 
+        //Delete Building link function
         private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Name, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Name, "Click" }); //save log data
             Program.GetParent().LoadScreen("DeleteBuilding");
         }
 
+        //load the Building panel
         private void BuildingsSidePanel_Load(object sender, EventArgs e)
         {
             this.Dock = DockStyle.Fill;
         }
 
+        //Manage Buildings link function
         private void labelHeader_Click(object sender, EventArgs e)
         {
             Program.GetParent().LoadScreen("DefaultBuilding");
