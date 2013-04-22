3d536061f52ba933484ec7afa0a9678a157e279e
KJ Jablonowski <kjablon@bgsu.edu>
2013-04-22 14:30:22 -0400

    Rooms side panel comments
    SIDE PANELS DONE LULZ

--- ElectronicRoomScheduler/SidePanels/RoomsSidePanel.cs
+++ ElectronicRoomScheduler/SidePanels/RoomsSidePanel.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* CS 3240 Electronic Room Scheduler
+ * Coded: Rob Risner
+ * Commented: KJ Jablonowski
+ * Due: 4/22/2013
+ * Rooms Side Menu
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -11,43 +18,50 @@ namespace ElectronicRoomScheduler.SidePanels
 {
     public partial class RoomsSidePanel : UserControl
     {
+        //create side panel
         public RoomsSidePanel()
         {
             InitializeComponent();
         }
 
+        //Add Room link function
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" }); //save log data
             Program.GetParent().LoadScreen("AddRoom");
         }
 
+        //fill panel with menu
         private void RoomsSidePanel_Load(object sender, EventArgs e)
         {
             this.Dock = DockStyle.Fill;
         }
 
+        //Edit Room link function
         private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" }); //save log data
             Program.GetParent().LoadScreen("EditRoom");
         }
 
+        //Delete Room link function
         private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" }); //save log data
             Program.GetParent().LoadScreen("DeleteRoom");
         }
 
+        //Request Technology link function
         private void linkLabelRequestTech_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" }); //save log data
             Program.GetParent().LoadScreen("RequestTech");
         }
 
+        //Manage Rooms link function
         private void labelHeader_Click(object sender, EventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((Label)sender).Text, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((Label)sender).Text, "Click" }); //save log data
             Program.GetParent().LoadScreen("DefaultRoom");
         }
     }
