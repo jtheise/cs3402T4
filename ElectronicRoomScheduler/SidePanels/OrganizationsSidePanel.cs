7a7f4b6455eeadf9a927dc016ed3144dc933e131
KJ Jablonowski <kjablon@bgsu.edu>
2013-04-22 14:24:59 -0400

    organizations side panel comments

--- ElectronicRoomScheduler/SidePanels/OrganizationsSidePanel.cs
+++ ElectronicRoomScheduler/SidePanels/OrganizationsSidePanel.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* CS 3240 Electronic Room Scheduler
+ * Coded: Rob Risner
+ * Commented: KJ Jablonowski
+ * Due: 4/22/2013
+ * Organizations Side Menu
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -11,37 +18,43 @@ namespace ElectronicRoomScheduler.SidePanels
 {
     public partial class OrganizationsSidePanel : UserControl
     {
+        //create side panel
         public OrganizationsSidePanel()
         {
             InitializeComponent();
         }
 
+        //fill side panel menu
         private void OrganizationsSidePanel_Load(object sender, EventArgs e)
         {
             this.Dock = DockStyle.Fill;
         }
 
+        //Add Organization link function
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" }); //save log data
             Program.GetParent().LoadScreen("AddOrganization");
         }
 
+        //Edit Organization link function
         private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" }); //save log data
             Program.GetParent().LoadScreen("EditOrganization");
         }
 
+        //Delete Organization link function
         private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((LinkLabel)sender).Text, "Click" }); //save log data
             Program.GetParent().LoadScreen("DeleteOrganization");
         }
 
+        //Manage Organizations link function
         private void labelHeader_Click(object sender, EventArgs e)
         {
-            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((Label)sender).Text, "Click" });
+            Program.LogButtonClick(new string[] { DateTime.Now.ToString(), ((Label)sender).Text, "Click" }); //save log function
             Program.GetParent().LoadScreen("DefaultOrganization");
         }
     }
